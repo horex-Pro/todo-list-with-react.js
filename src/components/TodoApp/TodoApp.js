d9572584d444494152e34548d7b1566454412288
horex-pro <hosseinhorex@gmail.com>
2023-07-21 16:53:06 +0330

    React-Select has added in this project

--- src/components/TodoApp/TodoApp.js
+++ src/components/TodoApp/TodoApp.js
@@ -3,12 +3,20 @@ import TodoForm from "../TodoForm/TodoForm";
 import TodoList from "../TodoList/TodoList";
 import styles from './TodoApp.module.css'
 import UncompletedCount from "../UncompletedCounter/UncompletedCounter";
+import Select from 'react-select';
+
+
+const options = [
+  { value: 'all', label: 'All' },
+  { value: 'Completed', label: 'Completed' },
+  { value: 'Uncompleted', label: 'Uncompleted' },
+];
 
 const TodoApp = () => {
     
     const [todos, setTodos] = useState([]);
     const [ filteredTodos, setFilteredTodos ] = useState( [] );
-    const [ status, setStatus ] = useState();
+    const [ status, setStatus ] = useState('all');
 
     useEffect(() => {
         const updatedTodos = changeHandler(status);
@@ -35,7 +43,7 @@ const TodoApp = () => {
     {
         
         setStatus(newStatus)
-        switch (status) {
+        switch (status.value) {
             case 'Completed':
                 return todos.filter(i => i.isCompleted);
             case 'Uncompleted':
@@ -47,23 +55,24 @@ const TodoApp = () => {
     }
 
     return (
-        <div className={styles.wrapper}>
+        <div className={ styles.wrapper }>
             <h1>Todo List App</h1>
-            <div className={styles.container}>
-                <div className={styles.row}>
-                    <UncompletedCount todos={todos} />
-                    {!todos.length ? <h2>Set Your Daily Todos</h2> : <h2>Uncompleted Task</h2>}
-                    <select onChange={(event) => setFilteredTodos(changeHandler(event.target.value))}>
-                        <option value='all'>All</option>
-                        <option value='Completed'>Completed</option>
-                        <option value='Uncompleted'>Uncompleted</option>
-                    </select>
+            <div className={ styles.container }>
+                <div className={ styles.row }>
+                    <UncompletedCount todos={ todos } />
+                    { !todos.length ? <h2>Set Your Daily Todos</h2> : <h2>Uncompleted Task</h2> }
+                    <Select
+                        defaultValue={ status }
+                        onChange={ setStatus }
+                        options={ options }
+                        className={styles.select}
+                    />
                 </div>
-                <TodoForm addTodo={addTodo} />
-                <TodoList todos={filteredTodos} onComplete={onCompletedHandler} onRemove={removeHandler} />
+                <TodoForm addTodo={ addTodo } />
+                <TodoList todos={ filteredTodos } onComplete={ onCompletedHandler } onRemove={ removeHandler } />
             </div>
         </div>
-    );
+    )
 }
 
 export default TodoApp;
